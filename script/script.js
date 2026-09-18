f80f9630a195c847a83209e5b897f4b5d5466b86
gabidut <misterlarry76@gmail.com>
2026-09-18 09:22:14 +0200

    feat: dynamic DOM generation for game board with data-attributes and a11y properties

--- script/script.js
+++ script/script.js
@@ -1,15 +1,28 @@
 const dimension = 150;
 const imgStart = Math.round(Math.random() * 1000);
 const randomImages = [];
+let cards = [...randomImages, ...randomImages];
+const cardsElement = document.querySelector('.cards');
 
 document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 8; i++) {
         randomImages.push(`https://picsum.photos/${dimension}/${dimension}?random=${imgStart + i}`)
     }
 
-    let cards = [...randomImages, ...randomImages];
-    shuffle(cards);
+    initGame();
 });
+
+function initGame() {
+    shuffle(cards);
+    cards.forEach((card, index) => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.dataset.value = card;
+        cardElement.setAttribute('role', 'button');
+        cardElement.setAttribute('tabindex', '0');
+        cardElement.appendChild(cardElement);
+    });
+}
 
 function shuffle(arr) {
     let i = arr.length, j, temp;
